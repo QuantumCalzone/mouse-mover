e1c67e296ea6703f7b4175de6fb1993f2d001b48
QuantumCalzone <QuantumCalzone@gmail.com>
2021-03-15 12:18:38 -0700

    setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mouse-mover',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/QuantumCalzone/mouse-mover',
+    license='GNU General Public License v3.0',
+    author='QuantumCalzone',
+    author_email='QuantumCalzone@gmail.com',
+    description='Allows you to slack off from Slack tracking your online status. Adapted from https://github.com/carrot69/keep-presence'
+)
